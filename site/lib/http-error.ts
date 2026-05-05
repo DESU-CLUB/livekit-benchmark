f495f729f0b2baf0c6d382c7e62956cc357d8ca5
DESU-CLUB <52738837+DESU-CLUB@users.noreply.github.com>
2026-05-05 05:45:57 +0000

    Deploying to gh-pages from @ DESU-CLUB/livekit-benchmark@d351ae63a6830195a640bbb786fb8431427c8f8a 🚀

--- site/lib/http-error.ts
+++
@@ -1,8 +0,0 @@
-export class HttpError extends Error {
-  readonly status?: number;
-
-  constructor(message: string, options?: { status?: number }) {
-    super(message);
-    this.status = options?.status;
-  }
-}
